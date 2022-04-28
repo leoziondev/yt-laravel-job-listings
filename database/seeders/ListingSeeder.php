835d9bcee845cc44667f3a823f65bbd228976ce3
LeoZion <leozionsuporte@gmail.com>
2022-04-28 11:59:31 -0300

    add listing and user seeder

---
+++ database/seeders/ListingSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Listing;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ListingSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Listing::factory(10)->create();
+    }
+}
