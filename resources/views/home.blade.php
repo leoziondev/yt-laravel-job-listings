d9f31fa4c0457f268c4873732232c9f6fefa3f9e
LeoZion <leozionsuporte@gmail.com>
2022-04-29 08:36:42 -0300

    refactor home view use components

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,51 +12,10 @@
             </div>
         @endif
         @if ($listings->count() > 0)
-            <div class="grid lg:grid-cols-2 gap-4">
+            <div class="grid lg:grid-cols-2 gap-6">
 
                 @foreach ($listings as $listing)
-                    <div class="bg-white rounded-md border-l-[6px] border-indigo-500 shadow-lg p-6">
-                        <div class="flex">
-                            <img
-                                class="hidden w-48 mr-6 md:block"
-                                src="images/wayne.png"
-                                alt=""
-                            />
-                            <div>
-                                <h2 class="text-xl text-gray-800 font-bold">
-                                    <a href="show.html">{{ $listing->title }}</a>
-                                </h2>
-                                <div class="text-md text-gray-600 font-bold mb-4">
-                                    {{ $listing->company }}
-                                </div>
-                                <ul class="flex">
-                                    <li
-                                        class="flex items-center justify-center bg-gray-800 text-white rounded-xl py-1 px-3 mr-2 text-xs"
-                                    >
-                                        <a href="#">Laravel</a>
-                                    </li>
-                                    <li
-                                        class="flex items-center justify-center bg-gray-800 text-white rounded-xl py-1 px-3 mr-2 text-xs"
-                                    >
-                                        <a href="#">API</a>
-                                    </li>
-                                    <li
-                                        class="flex items-center justify-center bg-gray-800 text-white rounded-xl py-1 px-3 mr-2 text-xs"
-                                    >
-                                        <a href="#">Backend</a>
-                                    </li>
-                                    <li
-                                        class="flex items-center justify-center bg-gray-800 text-white rounded-xl py-1 px-3 mr-2 text-xs"
-                                    >
-                                        <a href="#">Vue</a>
-                                    </li>
-                                </ul>
-                                <div class="text-base text-gray-600 mt-4">
-                                    <i class="fa-solid fa-location-dot"></i> {{ $listing->location }}
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                    <x-listing.card :listing="$listing" />
                 @endforeach
 
             </div>
